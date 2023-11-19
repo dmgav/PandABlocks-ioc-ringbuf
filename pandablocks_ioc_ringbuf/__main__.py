37c9726c34022328d99b3fc89a77572d77258b90
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2023-11-19 11:58:22 -0500

    ENH: first (not working) implementation

---
+++ pandablocks_ioc_ringbuf/__main__.py
@@ -0,0 +1,61 @@
+import logging
+
+import click
+from pandablocks.asyncio import AsyncioClient
+from ioc import create_softioc
+
+__all__ = ["cli"]
+
+
+@click.group(invoke_without_command=True)
+@click.option(
+    "--log-level",
+    default="INFO",
+    type=click.Choice(
+        ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], case_sensitive=False
+    ),
+)
+@click.version_option()
+@click.pass_context
+def cli(ctx, log_level: str):
+    """
+    PandaBlocks client library command line interface.
+    """
+    level = getattr(logging, log_level.upper(), None)
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=level)
+
+    # if no command is supplied, print the help message
+    if ctx.invoked_subcommand is None:
+        click.echo(cli.get_help(ctx))
+
+
+@cli.command()
+@click.argument("host")
+@click.argument("prefix")
+@click.option(
+    "--buffer-max-size",
+    type=int,
+    default=100000,
+    help="Maximum size of the ring buffer.",
+)
+def softioc(
+    host: str,
+    prefix: str,
+    buffer_max_size: int,
+):
+    """
+    Connect to the given HOST and create an IOC with the given PREFIX.
+    """
+    print(f"host: {host}")
+    print(f"prefix: {prefix}")
+    print(f"buffer_max_size: {buffer_max_size}")
+
+    create_softioc(
+        client=AsyncioClient(host),
+        record_prefix=prefix,
+        buffer_max_size=buffer_max_size,
+    )
+
+# test with: python -m pandablocks_ioc
+if __name__ == "__main__":
+    cli()
---
+++ pandablocks_ioc_ringbuf/_types.py
@@ -0,0 +1,50 @@
+from typing import Any, Awaitable, Callable, List, NewType, Optional, Union
+from softioc.pythonSoftIoc import RecordWrapper
+from dataclasses import dataclass, field
+from pandablocks.responses import FieldInfo
+
+class InErrorException(Exception):
+    """Placeholder exception to mark a field as being in error as reported by PandA"""
+
+# Custom type aliases and new types
+ScalarRecordValue = Union[str, InErrorException]
+TableRecordValue = List[str]
+RecordValue = Union[ScalarRecordValue, TableRecordValue]
+# EPICS format, i.e. ":" dividers
+EpicsName = NewType("EpicsName", str)
+# PandA format, i.e. "." dividers
+PandAName = NewType("PandAName", str)
+
+# Constants used in bool records
+ZNAM_STR = "0"
+ONAM_STR = "1"
+
+@dataclass
+class RecordInfo:
+    """A container for a record and extra information needed to later update
+    the record.
+
+    `record`: The PythonSoftIOC RecordWrapper instance. Must be provided
+        via the add_record() method.
+    `record_prefix`: The device prefix the record uses.
+    `data_type_func`: Function to convert string data to form appropriate for the record
+    `labels`: List of valid labels for the record. By setting this field to non-None,
+        the `record` is assumed to be mbbi/mbbo type.
+    `is_in_record`: Flag for whether the `record` is an "In" record type.
+    `on_changes_func`: Function called during processing of *CHANGES? for this record
+    `_pending_change`: Marks whether this record was just Put data to PandA, and so is
+        expecting to see the same value come back from a *CHANGES? request.
+    `_field_info`: The FieldInfo structure associated with this record. May be a
+        subclass of FieldInfo."""
+
+    record: RecordWrapper = field(init=False)
+    data_type_func: Callable
+    labels: Optional[List[str]] = None
+    # PythonSoftIOC issues #52 or #54 may remove need for is_in_record
+    is_in_record: bool = True
+    on_changes_func: Optional[Callable[[Any], Awaitable[None]]] = None
+    _pending_change: bool = field(default=False, init=False)
+    _field_info: Optional[FieldInfo] = field(default=None, init=False)
+
+    def add_record(self, record: RecordWrapper) -> None:
+        self.record = record
